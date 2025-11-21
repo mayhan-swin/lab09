6ff728805d0a1eaae7a490ed3582604bae95ebb7
mmnhan <106195423@student.swin.edu.au>
2025-11-21 14:42:15 +0700

    fixed cars and settings

---
+++ cars.php
@@ -0,0 +1,20 @@
+<?php
+require_once "settings.php";
+$dbconn=mysqli_connect($host, $user, $pwd, $sql_db);
+if($dbconn){
+    $query="SELECT * FROM cars";
+    $result=mysqli_query($dbconn,$query);
+        if($result){
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<tr>";
+                echo "<td>" . $row['car_id'] . "</td>";
+                echo "<td>" . $row['make'] . "</td>";
+                echo "<td>" . $row['model'] . "</td>";
+                echo "<td>" . $row['price'] . "</td>";
+                echo "<td>" . $row['yom'] . "</td>";
+                echo "</tr>";
+            }
+        }  else {echo "<p>There are no cars to display";}
+        mysqli_close($dbconn);
+    } else {echo "<p>Unable to connect to the db.</p>";}
+?>
